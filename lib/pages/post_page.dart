1dc5d36c3ea9334ff2d5bc2afddda0e59cfe1674
asadbek-ip <dasturchi6678@gmail.com>
2023-01-11 14:44:04 +0500

    post page

--- lib/pages/post_page.dart
+++ lib/pages/post_page.dart
@@ -1,6 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:instagramclone/model/post_model.dart';
 
 class PostPage extends StatefulWidget {
   const PostPage({super.key});
@@ -10,12 +11,134 @@ class PostPage extends StatefulWidget {
 }
 
 class _PostPageState extends State<PostPage> {
+  List<Post> posts = [];
+
+  final String _image_url1 =
+      "https://images.unsplash.com/photo-1673377006564-45c061e6cbe3?";
+  final String _image_url2 =
+      "https://images.unsplash.com/photo-1673364725341-7cd9a8332bd7?";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    posts.add(
+      Post(_image_url1, "Ajoyib bahor fasli daxshat"),
+    );
+    posts.add(Post(_image_url2, "Toylar muborak daxshat"));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Post Page"),
-      ),
+      appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  EvaIcons.camera,
+                  color: Colors.black,
+                  size: 30,
+                ))
+          ],
+          elevation: 0.1,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: const Text(
+            "Instagram",
+            style: TextStyle(
+                fontSize: 40, fontFamily: "Billabong", color: Colors.black),
+          )),
+      body: ListView.separated( itemBuilder: (context, index) => _itemPost(
+              context, posts[index].image_url, posts[index].caption!), separatorBuilder: (context,index)=>const Divider(thickness: 0.8,), itemCount: posts.length)
     );
   }
+}
+
+Widget _itemPost(BuildContext context, String imgUrl, String caption) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage("assets/images/profile_img.png"),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "User Name",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  "11-yanvar 2023",
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+              ],
+            ),
+            const Spacer(),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  EvaIcons.moreHorizontal,
+                  size: 30,
+                ))
+          ],
+        ),
+      ),
+      Container(
+        color: Colors.grey.withOpacity(0.3),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width * 2 / 3,
+        child: CachedNetworkImage(
+          imageUrl: imgUrl,
+          placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+        ),
+      ),
+      const SizedBox(
+        height: 5,
+      ),
+      Row(
+        children: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                EvaIcons.heartOutline,
+                size: 30,
+              )),
+          const SizedBox(
+            width: 5,
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                EvaIcons.paperPlane,
+                size: 30,
+              ))
+        ],
+      ),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Text(
+          caption,
+          style: const TextStyle(fontSize: 16),
+        ),
+      )
+    ],
+  );
 }
--- lib/pages/upload_page.dart
+++ lib/pages/upload_page.dart
@@ -38,114 +38,111 @@ class _UploadPageState extends State<UploadPage> {
 
   _showPic() {
     showModalBottomSheet(
-        context: context,
-        builder: (context) => Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ListTile(
-                  onTap: () {
-                    _showGallery();
-                  },
-                  leading: const Icon(Icons.photo_library),
-                  title: const Text("Pick Photo"),
-                ),
-                ListTile(
-                  onTap: () {
-                    _showCamera();
-                  },
-                  leading: const Icon(Icons.camera_alt_outlined),
-                  title: const Text("Take Photo"),
-                ),
-              ],
-            ));
+      context: context,
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ListTile(
+            onTap: () {
+              _showGallery();
+            },
+            leading: const Icon(Icons.photo_library),
+            title: const Text("Pick Photo"),
+          ),
+          ListTile(
+            onTap: () {
+              _showCamera();
+            },
+            leading: const Icon(Icons.camera_alt_outlined),
+            title: const Text("Take Photo"),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.add_photo_alternate,
-                  color: Color(0xfff833ab4),
-                ))
-          ],
-          centerTitle: true,
-          title: const Text(
-            "Upload",
-            style: TextStyle(
-                color: Colors.black, fontSize: 35, fontFamily: "Billabong"),
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.add_photo_alternate,
+                color: Color(0xfff833ab4),
+              ))
+        ],
+        centerTitle: true,
+        title: const Text(
+          "Upload",
+          style: TextStyle(
+              color: Colors.black, fontSize: 35, fontFamily: "Billabong"),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              InkWell(
-                onTap: () {
-                  _showPic();
-                },
-                child: Stack(
-                  children: [
-                    Container(
-                      color: Colors.grey.shade400,
-                      width: MediaQuery.of(context).size.width,
-                      height: MediaQuery.of(context).size.width,
-                      child: _image == null
-                          ? const Icon(
-                              Icons.add_a_photo,
-                              color: Color.fromARGB(255, 153, 153, 153),
-                              size: 50,
-                            )
-                          : Image(
-                              image: FileImage(_image!),
-                              fit: BoxFit.cover,
-                            ),
-                    ),
-                    _image != null
-                        ? Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                CircleAvatar(
-                                  backgroundColor: Colors.black,
-                                  child: IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        _image = null;
-                                      });
-                                    },
-                                    icon: const Icon(Icons.close),
-                                  ),
-                                ),
-                              ],
-                            ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                _showPic();
+              },
+              child: Stack(
+                children: [
+                  Container(
+                    color: Colors.grey.shade400,
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                    child: _image == null
+                        ? const Icon(
+                            Icons.add_a_photo,
+                            color: Color.fromARGB(255, 153, 153, 153),
+                            size: 50,
                           )
-                        : Container(),
-                  ],
-                ),
+                        : Image(
+                            image: FileImage(_image!),
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                  _image != null
+                      ? Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Colors.black,
+                                child: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _image = null;
+                                    });
+                                  },
+                                  icon: const Icon(Icons.close),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Container(),
+                ],
               ),
-              
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: TextField(
-                  keyboardType: TextInputType.multiline,
-                  minLines:1,
-                  maxLines: 5,
-                decoration: InputDecoration(
-                 
-                  hintText: "Caption"
-                ),
-                ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: TextField(
+                keyboardType: TextInputType.multiline,
+                minLines: 1,
+                maxLines: 5,
+                decoration: InputDecoration(hintText: "Caption"),
               ),
-            
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
